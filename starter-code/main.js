b1cc4a3743346646aaa5b2a878c51fd55e0aeb7b
Diego Arroyo <arroyodiego1@gmail.com>
2018-05-02 07:18:48 -0500

    v-bind

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,6 +4,7 @@ var app = new Vue({
     product: 'Socks',
     description: 'A pair of warm, fuzzy socks',
     image: './assets/vmSocks-green-onWhite.jpg',
-    altText: 'A pair of socks.'
+    altText: 'A pair of socks.',
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
   } 
 });
